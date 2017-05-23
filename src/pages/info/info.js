772917d421fe413eb46ce06624deb731e137c30d
Salvador Hernandez <zhavaman@gmail.com>
2017-05-23 15:51:34 -0500

    #6 add general information page model controler

---
+++ src/pages/info/info.js
@@ -0,0 +1,17 @@
+import {HttpClient} from '../../aurelia-http-client';
+
+let httpClient = new HttpClient();
+
+export class Info {
+  constructor() {
+    this.getData();
+  }
+
+  getData() {
+    httpClient.get('http://demo6292426.mockable.io/professionales-blick')
+    .then(data => {
+      this.questions = JSON.parse(data.response);
+      console.log(this.questions)
+    });
+  }
+}
