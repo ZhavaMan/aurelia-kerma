e6a384470e98435607144aa0805a485dbeeaee65
Salvador Hernandez <zhavaman@gmail.com>
2017-05-23 16:00:52 -0500

    #5 use import_tree plug in for stylus

--- aurelia_project/tasks/process-css.js
+++ aurelia_project/tasks/process-css.js
@@ -4,11 +4,15 @@ import sourcemaps from 'gulp-sourcemaps';
 import stylus from 'gulp-stylus';
 import project from '../aurelia.json';
 import {build} from 'aurelia-cli';
+import autoprefixerStylus from 'autoprefixer-stylus';
+import import_tree from 'stylus-import-tree';
+
 
 export default function processCSS() {
   return gulp.src(project.cssProcessor.source)
     .pipe(changedInPlace({firstPass: true}))
     .pipe(sourcemaps.init())
-    .pipe(stylus())
+    .pipe(stylus({use: [autoprefixerStylus('last 2 versions')],
+    	define: {import_tree: import_tree}}))
     .pipe(build.bundle());
 }
