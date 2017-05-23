d62c484d694f75a58c5f0ccec1952963c7bad135
Salvador Hernandez <zhavaman@gmail.com>
2017-05-23 16:02:56 -0500

    #7 add routering

--- src/app.js
+++ src/app.js
@@ -1,18 +1,30 @@
 export class App {
   constructor() {
+    
+  }
 
-   }
-
-  configureRouter(config, router){
+  configureRouter(config, router) {
     config.title = 'Kerma Partners';
 
     config.map([
-      { route: ['','home'],  name: 'resumen',
-        moduleId: './components/home/home',  nav: true, title: 'Resumen' },
-      { route: 'about',  name: 'about',
-        moduleId: './components/about/about', nav: true, title: 'About' },
-      { route: 'login',  name: 'login',
-        moduleId: './components/login/login', nav: true, title: 'Login' }
+      { route: ['/', '/resumen'],
+        name: 'summary',
+        moduleId: './pages/summary/summary',
+        nav: true,
+        title: 'Resumen'
+      },
+      { route: '/informacion-general',
+        name: 'info',
+        moduleId: './pages/info/info',
+        nav: true,
+        title: 'Información general'
+      },
+      { route: '/ingresos',
+        name: 'income',
+        moduleId: './pages/income-collection_income/income',
+        nav: true,
+        title: 'Ingresos - Ingresos Cobranza'
+      }
     ]);
 
     this.router = router;
